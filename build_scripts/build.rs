05cdc5b0bf62ded6ab5a968bf08c6bc15273131a
Suntharesan Mohan <suntharesan.mohan@gmail.com>
2020-07-03 15:57:42 -0400

    Badge refactor (#5)
    * Update deps
    
    * Impl Render trait for icon
    
    * Use const generics
    
    * Refactor icons to use phf map
    
    * Move icons svg file closer to build
    
    * Change to struct
    
    Co-authored-by: Suntharesan Mohan <suntharesan.mohan@openlane.com>

---
+++ build_scripts/build.rs
@@ -0,0 +1,49 @@
+// use phf_codegen::Map;
+use phf_codegen::Map;
+use scraper::{Html, Selector};
+use std::fs::File;
+use std::{
+  env,
+  io::{BufWriter, Write},
+  path::Path,
+};
+
+fn generate_icon_map() {
+  let path = Path::new(&env::var("OUT_DIR").unwrap()).join("icons_map.rs");
+  let selector = Selector::parse("symbol").unwrap();
+
+  let mut file = BufWriter::new(File::create(path).unwrap());
+
+  let mut map = Map::<&'static str>::new();
+
+  let doc = Html::parse_fragment(include_str!("./icons/brands.svg"));
+  for el in doc.select(&selector) {
+    let id = el.value().attr("id").unwrap();
+    let sym = el.html();
+    if id == "font-awesome-logo-full" {
+      continue;
+    }
+    map.entry(id, &format!(r##"r#"{}"#"##, sym));
+  }
+
+  let doc = Html::parse_fragment(include_str!("./icons/solid.svg"));
+  for el in doc.select(&selector) {
+    let id = el.value().attr("id").unwrap();
+    let sym = el.html();
+    if id == "font-awesome-logo-full" {
+      continue;
+    }
+    map.entry(id, &format!(r##"r#"{}"#"##, sym));
+  }
+
+  writeln!(
+    &mut file,
+    "static SYMBOLS: phf::Map<&'static str, &'static str> = \n{};\n",
+    map.build()
+  )
+  .expect("Failed to build icon map");
+}
+
+fn main() {
+  generate_icon_map()
+}
