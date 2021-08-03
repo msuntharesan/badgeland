67b8ad342d4c9a1727e446e90d30e7422fa69eed
Suntharesan Mohan <suntharesan.mohan@gmail.com>
2021-08-03 09:28:52 -0400

    Update icons,

--- scripts/download-simple-icons.ts
+++ scripts/download-simple-icons.ts
@@ -1,23 +1,11 @@
-import { cheerio } from "https://deno.land/x/cheerio@1.0.2/mod.ts";
-import icons from "https://cdn.skypack.dev/simple-icons/index.js";
-import {
-  join,
-  dirname,
-  fromFileUrl,
-} from "https://deno.land/std@0.89.0/path/mod.ts";
+import { join, dirname, fromFileUrl } from "https://deno.land/std@0.103.0/path/mod.ts";
 
-interface SimpleIcon {
-  title: string;
-  slug: string;
-  svg: string;
-  path: string;
-  source: string;
-  hex: string;
-}
-// <?xml version="1.0" encoding="UTF-8"?>
-const svgSprite = cheerio(
-  '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;"></svg>',
-);
+import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+import icons from "https://cdn.skypack.dev/simple-icons/index.js?dts";
+
+type SimpleIcon = { slug: string; svg: string };
+
+const svgSprite = cheerio('<svg xmlns="http://www.w3.org/2000/svg" style="display: none;"></svg>');
 
 for (const { slug, svg } of Object.values(icons) as SimpleIcon[]) {
   const $ = cheerio.load(svg);
@@ -26,24 +14,15 @@ for (const { slug, svg } of Object.values(icons) as SimpleIcon[]) {
 
   const path = cheerio.html($("svg > path"));
 
-  cheerio("<symbol></symbol>")
-    .attr("id", slug)
-    .attr("viewBox", vbox)
-    .append(path)
-    .appendTo(svgSprite);
+  cheerio("<symbol></symbol>").attr("id", slug).attr("viewBox", vbox).append(path).appendTo(svgSprite);
 }
 
 const __dirname = dirname(fromFileUrl(import.meta.url));
 
 const encoder = new TextEncoder();
 const svg = encoder.encode(
-  `
-  <?xml version="1.0" encoding="UTF-8"?>
-  ${cheerio.html(svgSprite, { xmlMode: true })}
-  `,
+  `<?xml version="1.0" encoding="UTF-8"?>
+${cheerio.html(svgSprite, { xmlMode: true })}`
 );
 
-Deno.writeFileSync(
-  join(__dirname, "../badgeland/build_scripts/icons/simple-icons.svg"),
-  svg,
-);
+Deno.writeFileSync(join(__dirname, "../badgeland/build_scripts/icons/simple-icons.svg"), svg);
